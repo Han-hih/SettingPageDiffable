02cdcb73fd347421b57a702007766882babfd3af
한 <hih458@naver.com>
2023-09-15 21:42:36 +0900

    settingPageDiffable 프로젝트 생성

---
+++ SettingPageDiffable/SettingPageDiffable/Hashable.swift
@@ -0,0 +1,12 @@
+//
+//  Hashable.swift
+//  SettingPageDiffable
+//
+//  Created by 황인호 on 2023/09/15.
+//
+
+import Foundation
+
+struct Table: Hashable {
+    let
+}
---
+++ SettingPageDiffable/SettingPageDiffable/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SettingPageDiffable
+//
+//  Created by 황인호 on 2023/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let list = [
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
