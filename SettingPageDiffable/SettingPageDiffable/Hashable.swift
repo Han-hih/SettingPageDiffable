fda57ce0361761bcfe1299f88b0e6059ec02ff4b
한 <hih458@naver.com>
2023-09-16 17:33:29 +0900

    add disclosureIndicator 추가
    Table구조체에 image, color 추가

--- SettingPageDiffable/SettingPageDiffable/Hashable.swift
+++ SettingPageDiffable/SettingPageDiffable/Hashable.swift
@@ -5,12 +5,18 @@
 //  Created by 황인호 on 2023/09/15.
 //
 
-import Foundation
 import UIKit
 
 struct Table: Hashable {
     
     let title: String
+    let image: String
+    let color: UIColor
     
+    init(title: String, image: String, color: UIColor) {
+        self.title = title
+        self.image = image
+        self.color = color
+    }
     
 }
